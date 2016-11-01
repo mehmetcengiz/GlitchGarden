77ddb9b004db67dcd73b0d62b652b06715177e34
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-01 23:59:18 +0300

    Animation Events Added to Attackers

--- Assets/Prefabs/Attackers/Attacker.cs
+++ Assets/Prefabs/Attackers/Attacker.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Attacker : MonoBehaviour {
 
     [Range(-1f,1.5f)]
-    public float walkSpeed;
+    public float CurrentSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +14,18 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	    transform.Translate(Vector3.left * CurrentSpeed * Time.deltaTime);
 	}
 
     void OnTriggerEnter2D() {
         Debug.Log(name + " trigger enter");
+    }
+
+    public void SetSpeed(float Speed) {
+        this.CurrentSpeed = Speed;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+        Debug.Log(name + " is attacking to target with damage: " + damage);
     }
 }
