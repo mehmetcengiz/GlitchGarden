a888cc42d459ac2d396b2c561a22ca7e2ed8502c
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-04 17:52:19 +0300

    Health component added.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,17 +4,21 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
-    private GameObject CurrentTarget;
-    private float CurrentSpeed;
+    private GameObject currentTarget;
+    private float currentSpeed;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-
+	    animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    transform.Translate(Vector3.left * CurrentSpeed * Time.deltaTime);
+	    transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	    if (!currentTarget) {
+	        animator.SetBool("isAttacking",false);
+	    }
 	}
 
     void OnTriggerEnter2D() {
@@ -22,15 +26,21 @@ public class Attacker : MonoBehaviour {
     }
 
     public void SetSpeed(float Speed) {
-        this.CurrentSpeed = Speed;
+        this.currentSpeed = Speed;
     }
 
     //Called from the animator at the time of actual blow
     public void StrikeCurrentTarget(float damage) {
-        Debug.Log(name + " is attacking to target with damage: " + damage);
+
+        if (currentTarget) {
+            Health health = currentTarget.GetComponent<Health>();
+            if (health) {
+                health.DealDamage(damage);
+            }
+        }
     }
 
     public void Attack(GameObject obj) {
-        CurrentTarget = obj;
+        currentTarget = obj;
     }
 }
