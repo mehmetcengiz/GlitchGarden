65e385b0a53c6142a5cb20aa1ce7f21272137762
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-13 02:49:42 +0300

    Shooters Deteckt Attackers

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,16 +6,62 @@ public class Shooter : MonoBehaviour {
     public GameObject projectile,gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
     void Start() {
-        projectileParent = GameObject.Find("Projectiles");
+        animator = GameObject.FindObjectOfType<Animator>();
+        
+        //Creates a parent if necessary
+        SetProjectileParent();
+        SetMyLaneSpawner();
+    }
 
-
-        if (!projectileParent) {
-            projectileParent = new GameObject("Projectiles");
+    void Update() {
+        if (IsAttackerAheadInLane()) {
+            animator.SetBool("isAttacking", true);
+        }
+        else {
+            animator.SetBool("isAttacking", false);   
         }
     }
 
+    //Look through all spawners, and set myLaneSpanwer if found.
+    void SetMyLaneSpawner() {
+        Spawner[] spawners = GameObject.FindObjectsOfType<Spawner>();
+        foreach (Spawner spawner in spawners) {
+            if (spawner.transform.position.y == transform.position.y) {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+        Debug.LogError(name + "can't find spawner in lane");
+
+    }
+
+    bool IsAttackerAheadInLane() {
+        //if there is no child.
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        }
+        //if there is attacker, are they ahead?
+        foreach (Transform attacker in myLaneSpawner.transform) {
+            if (attacker.transform.position.x > transform.position.x) {
+                return true;
+            }
+        }
+
+        //There is attacker but behind us.
+        return false;
+    }
+
+    void SetProjectileParent() {
+        projectileParent = GameObject.Find("Projectiles");
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject("Projectiles");
+        }
+    }
     private void Fire() {
         GameObject newProjectile = Instantiate(projectile);
         newProjectile.transform.parent = projectileParent.transform;
