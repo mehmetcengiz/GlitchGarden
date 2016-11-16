8eea7f9cb180830dff578f15004f3de107ba8b8c
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-11-16 22:25:08 +0300

    UI Slider Level Timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+
+    public float levelSecond = 100f;
+
+    private Slider slider;
+    private AudioSource audioSource;
+    private bool isLevelEnded = false;
+    private LevelManager levelManager;
+    private GameObject winMassage;
+
+    // Use this for initialization
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+        FİndYouWin();
+        winMassage.SetActive(false);
+    }
+
+    private void FİndYouWin() {
+        winMassage = GameObject.Find("You Win");
+        if (!winMassage) {
+            Debug.LogError("Please Create You Win GameObject.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        slider.value = Time.timeSinceLevelLoad/levelSecond;
+        if (Time.timeSinceLevelLoad >= levelSecond && !isLevelEnded) {
+            WinCondition();
+        }
+    }
+
+    private void WinCondition() {
+        audioSource.Play();
+        Invoke("LoadNextLevel", audioSource.clip.length);
+        isLevelEnded = true;
+        winMassage.SetActive(true);
+    }
+
+    void LoadNextLevel() {
+        levelManager.LoadNextLevel();
+    }
+}
